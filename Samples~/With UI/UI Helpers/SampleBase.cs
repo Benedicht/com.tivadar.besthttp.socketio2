e23bd6aa619cbae2d49602866b8307d9d538b6d5
Tivadar György Nagy <benedicht@gmail.com>
2023-07-21 14:36:37 +0200

    Initial Samples~ release

---
+++ Samples~/With UI/UI Helpers/SampleBase.cs
@@ -0,0 +1,27 @@
+using BestHTTP.Examples.Helpers.SelectorUI;
+
+using UnityEngine;
+
+namespace BestHTTP.Examples.Helpers
+{
+    abstract class SampleBase : MonoBehaviour
+    {
+#pragma warning disable 0649, 0169
+        [Header("Common Properties")]
+        public string Category;
+        public string DisplayName;
+
+        [TextArea]
+        public string Description;
+#pragma warning restore
+
+        public RuntimePlatform[] BannedPlatforms = new RuntimePlatform[0];
+
+        protected SampleSelectorUI sampleSelector;
+
+        protected virtual void Start()
+        {
+            this.sampleSelector = FindAnyObjectByType<SampleSelectorUI>();
+        }
+    }
+}
