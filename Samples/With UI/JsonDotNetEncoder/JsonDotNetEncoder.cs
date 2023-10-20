3e61b348ae9cf4fe7703ad6960fd6fb7dd381d56
Tivadar György Nagy <benedicht@gmail.com>
2023-10-20 19:39:46 +0200

    Moved encoder under 'With UI' folder

--- Samples/With UI/JsonDotNetEncoder/JsonDotNetEncoder.cs
+++ Samples/With UI/JsonDotNetEncoder/JsonDotNetEncoder.cs
@@ -1,3 +1,4 @@
+#if BESTHTTP_SOCKETIO_ENABLE_NEWTONSOFT_JSON_DOTNET_ENCODER
 using System;
 using System.Collections.Generic;
 
@@ -21,3 +22,4 @@ namespace Best.SocketIO.JsonEncoders
         }
     }
 }
+#endif
